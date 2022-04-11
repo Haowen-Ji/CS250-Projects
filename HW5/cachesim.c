07f51490fd47e747acffe2316e227c1e09e51ff2
Haowen Ji <jamesjhw2001@gmail.com>
2022-04-11 17:41:44 -0400

    new file

---
+++ HW5/cachesim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+const int word_size = 16;
+const int mem_size = 65536;
+const int block_size_max = 64;
